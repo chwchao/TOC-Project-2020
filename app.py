c7380b2ebb0f7589aa94b48e0c7d06ba417701b6
Chwchao <gerald.c.chao@gmail.com>
2019-12-10 23:54:24 +0800

    Add two arguments

--- app.py
+++ app.py
@@ -21,6 +21,8 @@ import time
 import datetime
 
 chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--disable-gpu')
+chrome_options.add_argument('--remote-debugging-port=9222')
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--disable-dev-shm-usage')
 chrome_options.add_argument('--no-sandbox')
