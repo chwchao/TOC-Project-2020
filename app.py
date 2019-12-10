2db1e779177882746193b119f1fb53ed73059fc0
Chwchao <gerald.c.chao@gmail.com>
2019-12-10 19:10:03 +0800

    Add login/register

--- app.py
+++ app.py
@@ -6,31 +6,58 @@ from dotenv import load_dotenv
 from linebot import LineBotApi, WebhookParser
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
+import pymongo
 
 from fsm import TocMachine
 from utils import send_text_message
 
 load_dotenv()
 
-
 machine = TocMachine(
-    states=["user", "state1", "state2"],
+    states=[
+        "visitor", "user", "naming"
+       # "course_add", "course_add_success", "course_add_failed"
+       # "course_delete", "course_delete_success", "course_delete_failed",
+       # "left_check"
+        ],
     transitions=[
         {
-            "trigger": "advance",
-            "source": "user",
-            "dest": "state1",
-            "conditions": "is_going_to_state1",
+            "trigger": "login",
+            "source": "visitor",
+            "dest": "user",
+            "conditions": "login",
         },
         {
-            "trigger": "advance",
-            "source": "user",
-            "dest": "state2",
-            "conditions": "is_going_to_state2",
+            "trigger": "register",
+            "source": "visitor",
+            "dest": "naming"
+        },
+        {
+            "trigger": "has_name",
+            "source": "check_name",
+            "dest": "user"
+        },
+        {
+            "trigger": "rename",
+            "source": "naming",
+            "dest": "user",
+            "conditions": "rename",
+        },
+        {
+            "trigger": "logout",
+            "source": ["visitor", "user", "check_name", "naming"],
+            "dest": "visitor",
+            "conditions": "logout",
+        },
+        {
+            "trigger": "state_check",
+            "source": ["visitor", "user"],
+            "dest": ["visitor", "user"],
+            "conditions": "state_check",
         },
         {"trigger": "go_back", "source": ["state1", "state2"], "dest": "user"},
     ],
-    initial="user",
+    initial="visitor",
     auto_transitions=False,
     show_conditions=True,
 )
@@ -41,15 +68,21 @@ app = Flask(__name__, static_url_path="")
 # get channel_secret and channel_access_token from your environment variable
 channel_secret = os.getenv("LINE_CHANNEL_SECRET", None)
 channel_access_token = os.getenv("LINE_CHANNEL_ACCESS_TOKEN", None)
+mongo_uri = os.getenv("MONGODB_URI", None)
 if channel_secret is None:
     print("Specify LINE_CHANNEL_SECRET as environment variable.")
     sys.exit(1)
 if channel_access_token is None:
     print("Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.")
     sys.exit(1)
+if mongo_uri is None:
+    print("Specify MONDODB_URI as environment variable.")
+    sys.exit(1)
 
 line_bot_api = LineBotApi(channel_access_token)
 parser = WebhookParser(channel_secret)
+client = pymongo.MongoClient(mongo_uri)
+coll_user = client.user
 
 
 @app.route("/callback", methods=["POST"])
@@ -83,8 +116,8 @@ def callback():
 def webhook_handler():
     signature = request.headers["X-Line-Signature"]
     # get request body as text
-    body = request.get_data(as_text=True)
-    app.logger.info(f"Request body: {body}")
+    # body = request.get_data(as_text=True)
+    # app.logger.info(f"Request body: {body}")
 
     # parse webhook body
     try:
@@ -102,6 +135,31 @@ def webhook_handler():
             continue
         print(f"\nFSM STATE: {machine.state}")
         print(f"REQUEST BODY: \n{body}")
+
+        def get_user(event):
+            user = coll_user.find_one({"id":event.userID})
+            return user
+        user = get_user(event)
+        user_state = ""
+        if user == None:
+            user_state = "visitor"
+        else:
+            user_state = user.state
+        machine.set_start(user_state)
+
+        if user_state == "visitor":
+            exist = machine.login(event)
+            if exist == True:
+                send_text_message(event.reply_token, "Login success!!")
+            else:
+                machine.register()
+
+            
+
+        
+        
+            
+        
         response = machine.advance(event)
         if response == False:
             send_text_message(event.reply_token, "Not Entering any State")
--- fsm.py
+++ fsm.py
@@ -2,10 +2,31 @@ from transitions.extensions import GraphMachine
 
 from utils import send_text_message
 
-
 class TocMachine(GraphMachine):
     def __init__(self, **machine_configs):
         self.machine = GraphMachine(model=self, **machine_configs)
+
+    def login(self, event):
+        user = coll_user.find_one({"id":event.userID})
+        if user == None:
+            newUser = {
+                "id" : event.userID,
+                "name" : "",
+                "target" : [],
+                "state" : "naming"
+            }
+            coll_user.insert_one(newUser)
+            return False
+        else:
+            user.state = "user"
+            result = collection.update_one({"id":event.userID}, {'$set': user})
+            return True
+
+    def set_start(self, name):
+        self.startState = name
+
+    def logout(self, event):
+        return True
 
     def is_going_to_state1(self, event):
         text = event.message.text
