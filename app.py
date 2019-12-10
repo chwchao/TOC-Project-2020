13532d271f54fa4835576722b4e86cd6ae13cd45
Chwchao <gerald.c.chao@gmail.com>
2019-12-10 23:41:46 +0800

    Add chrome arguments

--- app.py
+++ app.py
@@ -22,6 +22,8 @@ import datetime
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--disable-gpu')
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--disable-dev-shm-usage')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.binary_location = os.getenv("GOOGLE_CHROME_BIN", None)
 
