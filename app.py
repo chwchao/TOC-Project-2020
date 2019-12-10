3fc22cdb2b4c2a3f1d5c6df817cfcb17dd35e5bd
Chwchao <gerald.c.chao@gmail.com>
2019-12-11 00:50:26 +0800

    Remove browser closing

--- app.py
+++ app.py
@@ -241,7 +241,6 @@ def webhook_handler():
                         if browser.find_element_by_xpath("//tr[" + str(count) + "]//td[3]").text == course:
                             break
                         count = count + 1
-                    browser.quit()
                     return browser.find_element_by_xpath("//tr[" + str(count) + "]//td[17]").text
 
                 if len(user["target"]) == 0:
